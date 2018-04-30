0b02ae05187ccc57ff64765f9aabd0ba558eee6b
Setor Agodo <setor.k.agodo@gmail.com>
2018-04-30 16:45:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app = "I don't do much."
 
 var kittens = ["Milo", "Otis", "Garfield"]
 
-function destructivelyAppendKitten(kittens){
+function destructivelyAppendKitten(Ralph){
     (kittens).push('Ralph')
     return kittens
   }
