36e173828f6bd463ef581237767943acf04e73c2
Setor Agodo <setor.k.agodo@gmail.com>
2018-04-30 17:20:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,3 +29,11 @@ function appendKitten(Broom){
 function prependKitten(Arnold){
   return [Arnold, ...kittens]
 }
+
+function removeLastKitten(){
+  return kittens.slice [0, kittens.length - 1]
+}
+
+function removeFirstKitten(){
+  return kittens.slice(1)
+}
